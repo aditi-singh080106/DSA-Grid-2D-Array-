e08006c26442e8e6ea896ac99c464996c52f6fa6
Aditi Singh <aditi08092003@gmail.com>
2025-07-13 13:25:33 +0530

    Create SumOfAllElements.java

---
+++ SumOfAllElements.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Main {
+    private static void print(int[][] arr){
+        for(int i = 0 ; i < arr.length ; i++ ){
+            for(int j = 0 ; j < arr[0].length ; j++ ){
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entet the row size : ");
+        int m = sc.nextInt();
+        System.out.println("Enter the column size : ");
+        int n = sc.nextInt();
+        int[][] arr = new int[m][n];
+        System.out.println("Enter the element in  the matrix : ");
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++ ){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        print(arr);
+        int sum = 0 ;
+        for(int i = 0 ; i < m ; i++ ){
+            for(int j = 0 ; j < n ; j++ ){
+                 sum+=arr[i]j];
+            }
+        }
+        System.out.println("Sum of all element is : "+sum);
+    }
+}
